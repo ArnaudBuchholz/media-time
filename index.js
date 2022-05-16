ddc1ccb463bb228d8c59ceb7458d14e55e1728be
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-05-15 23:15:17 -0400

    Seems to be good

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const { readdir, open } = require('fs/promises')
+const { readdir } = require('fs/promises')
 const { extname, join } = require('path')
-const { utimes } = require('utimes');
+const { utimes } = require('utimes')
+const { exiftool } = require("exiftool-vendored")
 
 async function main (folder) {
   const files = await readdir(folder)
   for await (file of files) {
     const extension = extname(file).toLowerCase()
     const filePath = join(folder, file)
-    if (extension === '.jpg') {
-      let fh
-      let time
+    if (['.jpg', '.mov'].includes(extension)) {
       try {
-        fh = await open(filePath)
-        const buffer = Buffer.alloc(65535)
-        await fh.readv([buffer])
-        const parser = require('exif-parser').create(buffer)
-        const { DateTimeOriginal } = parser.parse().tags
-        const utcDate = new Date(DateTimeOriginal * 1000)
+        const tags = await exiftool.read(filePath)
+        let tagName = {
+          '.jpg': 'DateTimeOriginal',
+          '.mov': 'MediaCreateDate'
+        }[extension]
+        const exifDate = tags[tagName]
+        if (extension === '.mov') {
+          exifDate.minute += exifDate.tzoffsetMinutes // Bug ?
+        }
         const localDate = new Date(
-          utcDate.getUTCFullYear(), utcDate.getUTCMonth(), utcDate.getUTCDate(),
-          utcDate.getUTCHours(), utcDate.getUTCMinutes(), utcDate.getUTCSeconds(), 0
+          exifDate.year, exifDate.month - 1, exifDate.day,
+          exifDate.hour, exifDate.minute, exifDate.second, 0
         )
-        time = localDate.getTime()
-        console.log(file, localDate)
-      } catch (e) {
-        console.log(file, e)
-      } finally {
-        fh.close()
+        console.log(file, localDate.toLocaleString())
+        const time = localDate.getTime()
         await utimes(filePath, {
           btime: time,
           mtime: time,
           atime: time
         })
+      } catch (e) {
+        console.log(file, e)
       }
     }
   }
